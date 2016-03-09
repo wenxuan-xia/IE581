2d5e4178df6676226a0ddfb0e8c714b8148e9c33
wenxuan-xia <wenxuan.xia@hotmail.com>
2016-03-08 23:05:46 -0500

    hw3

--- hw3/prob_4/s4.py
+++ hw3/prob_4/s4.py
@@ -1,11 +1,23 @@
-import numpy
+import numpy as np
+import u16807d
 
 def intensityFunction(t):
     return 3+4.0/(t+1)
 
-
-def main():
+def NHPP(num):
+    rst = []
     LAMBDA = 7
+    s = 0
+    seed = 1234
+    while num>0:
+        seed, u1 = u16807d.u16807d(seed)
+        lmbd = intensityFunction(s)
+        s += -1/LAMBDA*np.log(u1)
+        seed, u2 = u16807d.u16807d(seed)
+        if u2*LAMBDA<intensityFunction(s):
+            rst.append(s)
+            num -= 1
+    return rst
 
 if __name__ == '__main__':
-    main()
+    print NHPP(10)
